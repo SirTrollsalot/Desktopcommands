7ea68c17eb507f5cd8208de610627cd8f2b4da3f
CommitProjectNotCrime <maksimovicmarko23@gmail.com>
2018-08-08 16:30:25 +0200

    Finished first stable version of Command_Run.cs

--- Desktopcommands/Commands/Command_Run.cs
+++ Desktopcommands/Commands/Command_Run.cs
@@ -1,22 +1,50 @@
-﻿using Desktopcommands.ResponseFields;
+﻿using Desktopcommands.Commands.CommandUtilities;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Input;
 
 namespace Desktopcommands.Commands
 {
     public class Command_Run : Command
     {
-        public Command_Run(string cname) : base("Command_Run")
+        string term;
+  
+        public Command_Run(string arguments) : base("Command_Run")
         {
-
+            term = arguments;
         }
 
         public override void Execute()
         {
-            throw new NotImplementedException();
+            ResponseFields.ResponseBox.SetItems(RegistryUtils.GetUninstallDisplayNamesFiltered(term));
+            ResponseFields.ResponseBox.KeyDown(SelectInstallationHandler);
+        }
+
+        public void SelectInstallationHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ResponseFields.ResponseBox.SetItems(RegistryUtils.GetExecutablePathsFromDisplayUninstallDisplayName(ResponseFields.ResponseBox.SelectedItem()));
+                ResponseFields.ResponseBox.KeyDown(RunExecutableHandler);
+            }
+            if (e.Key == Key.Escape)
+            {
+                MainWindow.AppWindow.Done();
+            }
+        }
+
+        public void RunExecutableHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Process.Start((string)ResponseFields.ResponseBox.SelectedItem());
+                MainWindow.AppWindow.Done();
+            }
+            if (e.Key == Key.Escape)
+            {
+                Execute(); //https://www.youtube.com/watch?v=uACvFAm6JSM
+            }
         }
     }
 }
