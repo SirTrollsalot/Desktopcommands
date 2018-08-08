c2fd28353438b7e22bbaca98dd0bc83bf3c4b09b
CommitProjectNotCrime <maksimovicmarko23@gmail.com>
2018-08-08 12:49:38 +0200

    started implementing Command_Run.cs

---
+++ Desktopcommands/Commands/Command_Run.cs
@@ -0,0 +1,22 @@
+﻿using Desktopcommands.ResponseFields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktopcommands.Commands
+{
+    public class Command_Run : Command
+    {
+        public Command_Run(string cname) : base("Command_Run")
+        {
+
+        }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
